1bfe9581fd9e5db035f831c2d3942b2692b75156
mulan <mulan114@yahoo.com>
2019-04-26 02:38:44 -0400

    completed assignment

--- models.js
+++ models.js
@@ -1,71 +1,30 @@
 const uuid = require('uuid');
+const mongoose = require("mongoose");
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
-
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
-
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    console.log('creating blog post');
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
+// this is our schema to represent a restaurant
+const blogSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: {
+    firstName: String,
+    lastName: String
   }
+});
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.author
+  };
 };
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blog = mongoose.model('Blog', blogSchema);
 
-
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = { Blog };
